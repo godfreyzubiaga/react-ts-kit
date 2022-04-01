719b5136802439c931a627ed11f7497fe90bcd07
Godfrey Zubiaga <godfrey.zubiaga@fpscinc.com>
2022-04-02 06:01:39 +0800

    fix: change ReactJS to ReactTS

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 
 const App = () => (
   <div className="container">
-    <h1 id="title">This is a ReactJS x Vite x PWA Boilerplate</h1>
+    <h1 id="title">This is a ReactTS x Vite x PWA Boilerplate</h1>
     <h2 id="subtext">
       Starting a React PWA project made easier with zero-configuration boilerplate.
     </h2>
     <span id="line" />
     <div id="instruction">
-      <p>Edit src/App.jsx to start</p>
+      <p>Edit src/App.tsx to start</p>
       <p>Edit src/App.css to change styles</p>
       <p>Edit manifest.json to change pwa data</p>
     </div>
