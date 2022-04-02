5de5cf224a59a82bfb77acea84e570f8fa22df87
Godfrey Zubiaga <godfrey.zubiaga@fpscinc.com>
2022-04-03 05:30:59 +0800

    fix: update landing page

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,38 @@ const App = () => (
   <div className="container">
     <h1 id="title">This is a ReactTS x Vite x PWA Boilerplate</h1>
     <h2 id="subtext">
-      Starting a React PWA project made easier with zero-configuration boilerplate.
+      Starting a React PWA project made easier with zero-configuration
+      boilerplate.
     </h2>
-    <span id="line" />
+    <div className="line" />
     <div id="instruction">
+      <p>
+        <a
+          className="link"
+          href="https://www.npmjs.com/package/create-react-ts-kit"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <code>npm install create-react-ts-kit</code>
+        </a>
+      </p>
+      <div className="line" />
       <p>Edit src/App.tsx to start</p>
       <p>Edit src/App.css to change styles</p>
       <p>Edit manifest.json to change pwa data</p>
     </div>
     <div id="react-logo" />
     <div id="information">
-      <p>
-        More information about ReactJS @
-        <a className="link" href="https://reactjs.org/" target="_blank" rel="noreferrer">
-          Official ReactJS Website
-        </a>
-      </p>
       <p id="footer">
-        You can know more about me @
+        Know more about me @
+        {' '}
         <a
           className="link"
           href="http://www.godfreyzubiaga.com/"
           target="_blank"
           rel="noreferrer"
         >
-          My Portfolio
+          Godfrey Zubiaga
         </a>
       </p>
     </div>
